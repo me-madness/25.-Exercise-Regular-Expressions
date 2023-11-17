e92df8f005b88f489f4a404bf7d8b9c7ef62b7b9
me_madness <p_rangel@abv.bg>
2023-11-17 20:11:44 +0100

    06. Done all task from me and the lecture

--- Exercise/06. extract_the_links.py
+++ Exercise/06. extract_the_links.py
@@ -2,7 +2,15 @@
 
 import re
 
+text = input()
+pattern = r"(w{3}\.[A-Za-z0-9\-\.]+\.[a-z]+)"
 
+while text:
+    match = re.search(pattern, text)
+    if match:
+        valid_url = match.group(1)
+        print(valid_url)
+    text = input()
 
 
 # Second task from me
