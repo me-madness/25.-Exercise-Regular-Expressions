7f9434350a303b2b2df799b1e9c89bf0088650c6
me_madness <p_rangel@abv.bg>
2023-11-17 19:12:30 +0100

    04. It's fucking ready

--- Exercise/04. extract_emails.py
+++ Exercise/04. extract_emails.py
@@ -3,9 +3,10 @@
 import re
 
 sentence = input()
-pattern = r""
-matches = re.findall(pattern, sentence)
-print(matches)
+pattern = r"\s(([a-z0-9]+[a-z0-9\.\-\_]*)@([a-z\-]+)(\.[a-z]+)+)\b"
+extracted_emails = re.findall(pattern, sentence)
+for extracted_email in extracted_emails:
+    print(extracted_emails[0])
 
 
 # Second task from me
