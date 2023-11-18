d0d38e01e8eb13b32998ce3a8369c32444a941f8
me_madness <p_rangel@abv.bg>
2023-11-18 18:24:41 +0100

    01. start with while loop

--- More Exercises/01. race.py
+++ More Exercises/01. race.py
@@ -1,6 +1,8 @@
 list_of_participants = input().split(",")
 commands = input()
 pattern = r"(([A-Za-z]+)|([0-9]+))"
-
+distance = {}
 while commands != "end of race":
+    commands = input()
+    matches = re.findall(pattern, commands)
     
