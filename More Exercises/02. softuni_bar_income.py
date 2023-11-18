b74c523f05b69caf534d943ff90152f3b608b411
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-18 22:59:50 +0100

    commi

--- More Exercises/02. softuni_bar_income.py
+++ More Exercises/02. softuni_bar_income.py
@@ -7,9 +7,9 @@ while command != "end of shift":
     match = re.search(pattern, command)
     if match:
         customer_name, product, quantity, prise = match.groups()
-        sum = int(quantity) * int(prise)
+        sum = int(quantity) * float(prise)
         total_sum += sum
-        print(f"{customer_name}: {product} - {sum}")
+        print(f"{customer_name}: {product} - {sum:.2f}")
     command = input()
 print(f"total income: {total_sum:.2f}")    
     
