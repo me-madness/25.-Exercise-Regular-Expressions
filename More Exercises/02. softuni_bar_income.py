5c2104290657e313f4c012f6a499cbc89d87c68f
me_madness <p_rangel@abv.bg>
2023-11-18 20:24:34 +0100

    02 it's done maybe

--- More Exercises/02. softuni_bar_income.py
+++ More Exercises/02. softuni_bar_income.py
@@ -1,11 +1,16 @@
 import re
 
-pattern = r""
+total_sum = 0
+pattern = r"(%([A-Za-z]+)%<([A-Za-z]+)>\|(\d)\|(\d+\.\d+)\$)"
 command = input()
 while command != "end of shift":
-    matches = re.findall(pattern, command)
-    
-    
-    
-    
+    match = re.search(pattern, command)
+    if match:
+        customer_name, product, quantity, prise = match.groups()
+        sum = int(quantity) * int(prise)
+        total_sum += sum
+        print(f"{customer_name}: {product} - {sum}")
     command = input()
+print(f"total income: {total_sum:.2f}")    
+    
+    
