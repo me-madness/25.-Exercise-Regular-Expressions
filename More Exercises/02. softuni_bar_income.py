71905191074d44d572e0d579b8c88ed9c4650b13
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-18 23:09:03 +0100

    I need to prepare new pattern

--- More Exercises/02. softuni_bar_income.py
+++ More Exercises/02. softuni_bar_income.py
@@ -1,7 +1,8 @@
 import re
 
 total_sum = 0
-pattern = r"%([A-Za-z]+)%<([A-Za-z]+)>\|(\d)\|(\d+\.\d+)\$"
+# pattern = r"%([A-Za-z]+)%<([A-Za-z]+)>\|(\d)\|(\d+\.\d+)\$"
+pattern = r"%([A-Z][a-z]+)%<([A-Za-z]+)>\|(\d)\|(\d+\.\d+)\$"
 command = input()
 while command != "end of shift":
     match = re.search(pattern, command)
