35647a22e035fcdfbfebf3228efde35d7026c3a3
me_madness <p_rangel@abv.bg>
2023-11-18 19:45:17 +0100

    new commit with one more if else

--- More Exercises/01. race.py
+++ More Exercises/01. race.py
@@ -4,18 +4,19 @@ list_of_participants = input().split(",")
 pattern_name = r"[A-Za-z]+"
 pattern_distance = r"\d"
 distance = {}
-new_score = 0
 commands = input()
 while commands != "end of race":
+    new_score = 0
     matches_name = re.findall(pattern_name, commands)
     matches_distance = re.findall(pattern_distance, commands)
     name = "".join(matches_name)
     scores = "".join(matches_distance)
-    if name in list_of_participants:
-        for score in scores:
-            common_score = score
-            new_score += int(common_score)
-        distance[name] = new_score    
-    print(distance)
+    for participant in list_of_participants:
+        if name == participant:
+            for score in scores:
+                common_score = score
+                new_score += int(common_score)
+            distance[name] = new_score    
+            print(distance)
     commands = input()
     
