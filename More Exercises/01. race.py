26cbd50d40857c0b192f8985e8ac2369b2b287b0
me_madness <p_rangel@abv.bg>
2023-11-18 19:24:11 +0100

    now score are work correct

--- More Exercises/01. race.py
+++ More Exercises/01. race.py
@@ -2,17 +2,19 @@ import re
 
 list_of_participants = input().split(",")
 pattern_name = r"[A-Za-z]+"
-pattern_distance = r"[0-9]+"
+pattern_distance = r"\d"
 distance = {}
+new_score = 0
 commands = input()
 while commands != "end of race":
     matches_name = re.findall(pattern_name, commands)
     matches_distance = re.findall(pattern_distance, commands)
     distance = "".join(matches_name)
-    score = ",".join(matches_distance)
-    commands = input()
-    print(distance)    
-    print(score)
+    scores = "".join(matches_distance)
+    for score in scores:
+        common_score = score
+        new_score += int(common_score)
+    distance[0] = new_score
     
     if matches_name not in list_of_participants:
         pass
