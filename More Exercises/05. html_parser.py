4fdb1746fd9ea273bec741ace73a0a6266ab00bc
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-19 14:04:38 +0100

    first pattern it's ready

--- More Exercises/05. html_parser.py
+++ More Exercises/05. html_parser.py
@@ -1,5 +1,7 @@
 import re
 
 data = input()
-pattern = r""
-match = re.search(pattern, data)
+pattern_title = r"<title>([A-Za-z\ ]+)<\/title>"
+pattern_content = r""
+match_title = re.findal(pattern_title, data)
+match_content = re.findal(pattern_content, data)
