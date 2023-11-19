f2b3668da2b59870720f21987d67a4fef0ce1e8f
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-19 14:29:51 +0100

    title it's done

--- More Exercises/05. html_parser.py
+++ More Exercises/05. html_parser.py
@@ -5,3 +5,4 @@ pattern_title = r"<title>([A-Za-z\ ]+)<\/title>"
 pattern_content = r""
 match_title = re.findal(pattern_title, data)
 match_content = re.findal(pattern_content, data)
+print(f"{''.join(match_title)}")
