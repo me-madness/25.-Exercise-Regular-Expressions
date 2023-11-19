90e5b4109e39fb21560348d0cb59a0dcf1b546c9
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-19 14:32:51 +0100

    Title it's really done,a need to fix the pattern for content

--- More Exercises/05. html_parser.py
+++ More Exercises/05. html_parser.py
@@ -3,6 +3,6 @@ import re
 data = input()
 pattern_title = r"<title>([A-Za-z\ ]+)<\/title>"
 pattern_content = r""
-match_title = re.findal(pattern_title, data)
-match_content = re.findal(pattern_content, data)
-print(f"{''.join(match_title)}")
+match_title = re.findall(pattern_title, data)
+match_content = re.findall(pattern_content, data)
+print(f"Title: {''.join(match_title)}")
