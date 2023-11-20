64e4d1ce5c475d04b6885f2a75e51e32b6d6d328
me_madness <p_rangel@abv.bg>
2023-11-20 18:12:02 +0100

    new commit

--- More Exercises/04.nether_realms.py
+++ More Exercises/04.nether_realms.py
@@ -1,6 +1,8 @@
 import re
 
 demon_name = input()
-pattern = r"(([A-Za-z]+)|([\-0-9\.]))"
-match = re.findall(pattern, demon_name)
-print(match)
+# pattern = r"(([A-Za-z]+)?([\-0-9\.]))"
+pattern = r"([A-Za-z]+)([0-9\.\-]+)"
+match = re.match(pattern, demon_name)
+name = match.group(1)
+damage_digits = match.group(2)
