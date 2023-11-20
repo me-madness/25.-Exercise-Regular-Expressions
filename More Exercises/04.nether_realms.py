394be0092f5662e0f6630d24c128fb7bef642bed
me_madness <p_rangel@abv.bg>
2023-11-20 18:15:50 +0100

    I try to fix my bug with the pattern

--- More Exercises/04.nether_realms.py
+++ More Exercises/04.nether_realms.py
@@ -3,6 +3,7 @@ import re
 demon_name = input()
 # pattern = r"(([A-Za-z]+)?([\-0-9\.]))"
 pattern = r"([A-Za-z]+)([0-9\.\-]+)"
-match = re.match(pattern, demon_name)
-name = match.group(1)
-damage_digits = match.group(2)
+match = re.search(pattern, demon_name)
+name, damage_digits = match.groups()
+print(name)
+print(damage_digits)
