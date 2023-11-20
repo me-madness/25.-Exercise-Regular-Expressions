d3dd7965be2752ab447155a8c33dd9956c469339
me_madness <p_rangel@abv.bg>
2023-11-20 18:29:20 +0100

    shit

--- More Exercises/04.nether_realms.py
+++ More Exercises/04.nether_realms.py
@@ -2,7 +2,7 @@ import re
 
 demon_name = input()
 # pattern = r"(([A-Za-z]+)?([\-0-9\.]))"
-pattern = r"([A-Za-z]+)([0-9\.\-]+)"
+pattern = r"(([A-Za-z]+)([0-9\.\-]+))"
 match = re.search(pattern, demon_name)
 name, damage_digits = match.groups()
 print(name)
